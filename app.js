dec2e5633a6e07653eb4097fa7efe92285942e73
swetha <swetha.ramdurg@skillrary.com>
2021-05-12 10:55:02 +0530

    third commit with spell fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Swetha Ramdurg,trying for the seond time!'));
+app.get('/', (req, res) => res.send('Hello Swetha Ramdurg,trying for the second time!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
